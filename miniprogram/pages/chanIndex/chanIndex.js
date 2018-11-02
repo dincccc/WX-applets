7b32c22500d952ffa5f9f3e5f1c5b01faa2fcdd2
dinchboom <dinchboom@gmail.com>
2018-11-02 17:47:15 +0800

    开始chan主页面编码

--- miniprogram/pages/chanIndex/chanIndex.js
+++ miniprogram/pages/chanIndex/chanIndex.js
@@ -5,82 +5,109 @@ Page({
    * 页面的初始数据
    */
   data: {
-    searchStyleIcon: "position: absolute; left: 244rpx;",
-    searchStyleInput: "text-align: center;",
-
+    //轮播图数据
     carBox: [{
-      imgUrl: '../c-imgs/banner-01.jpg',
-      linkTo: '',
-      imgAlt: '',
-    }],
+        imgUrl: '../../c-imgs/banner-01.jpg',
+        linkTo: '',
+        imgAlt: 'banner-01',
+      }, {
+        imgUrl: '../../c-imgs/banner-01.jpg',
+        linkTo: '',
+        imgAlt: 'banner-01',
+      },
+      {
+        imgUrl: '../../c-imgs/banner-01-xx.jpg',
+        linkTo: '',
+        imgAlt: 'banner-01',
+      },
+      {
+        imgUrl: '../../c-imgs/banner-01-xx.jpg',
+        linkTo: '',
+        imgAlt: 'banner-01',
+      },
+      {
+        imgUrl: '../../c-imgs/banner-01-xx.jpg',
+        linkTo: '',
+        imgAlt: 'banner-01',
+      }
+    ],
+    //四个版块入口
+    fourItems: [{
+        name: "景区",
+        url: "#",
+        imgUrl: "../../c-imgs/scenic-icon.png"
+      },
+      {
+        name: "住宿",
+        url: "#",
+        imgUrl: "../../c-imgs/live-icon.png"
+      },
+      {
+        name: "攻略",
+        url: "#",
+        imgUrl: "../../c-imgs/raiders-icon.png"
+      },
+      {
+        name: "购物",
+        url: "#",
+        imgUrl: "../../c-imgs/shopping-icon.png"
+      },
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
-
-  backLeft: function () {
-    this.setData({
-      searchStyleIcon: "margin-left: 10rpx",
-      searchStyleInput: "text-align: left;",
-    })
-  },
-  backCenter: function () {
-    this.setData({
-      searchStyleIcon: "position: absolute; left: 244rpx;",
-      searchStyleInput: "text-align: center;",
-    })
-  }
 })
